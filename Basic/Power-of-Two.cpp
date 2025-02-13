a7a9fe455d07d76cd8206cc0795652e566b31fb3
Neelanshu Mitra <mitraneelanshu@gmail.com>
2025-02-14 01:10:31 +0530

    Update Power-of-Two.cpp

--- Basic/Power-of-Two.cpp
+++ Basic/Power-of-Two.cpp
@@ -1,1 +1,13 @@
-
+// https://leetcode.com/problems/power-of-two/
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        for (int i=0;i<31;i++){
+            // int ans=pow(2,i);
+            if(n==pow(2,i)){
+                return true;
+            }
+        }
+        return false;
+    }
+};
